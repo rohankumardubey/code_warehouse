cdd82ccf0362f9c2f8a1bcd727958dc7497db187
trueeyu <lxhhust350@qq.com>
2021-09-23 17:22:01 +0800

    modify hook

--- hook_malloc/main.cpp
+++ hook_malloc/main.cpp
@@ -46,18 +46,21 @@ void operator delete(void* p, const std::nothrow_t& nt) noexcept {
   tc_delete_nothrow(p, nt);
 }
 
+// auto* c1 = new C1[5];
 void* operator new[](size_t size) {
   void* p = tc_newarray(size);
   std::cout<<"NEW_ARRAY_WITH_SIZE_THROW: "<<p<<":"<<size<<":"<<tc_nallocx(size, 0)<<std::endl;
   return p;
 }
 
+// auto* c1 = new (std::nothrow) C1[5];
 void* operator new[](size_t size, const std::nothrow_t& nt) noexcept {
   void* p = tc_newarray_nothrow(size, nt);
   std::cout<<"NEW_ARRAY_WITH_SIZE_NOT_THROW: "<<p<<":"<<size<<":"<<tc_nallocx(size, 0)<<std::endl;
   return p;
 }
 
+// delete []c1;
 void operator delete[](void* p) noexcept {
   std::cout<<"FREE_ARRAY_NO_THROW: "<<p<<":"<<tc_malloc_size(p)<<std::endl;
   tc_deletearray(p);
@@ -69,23 +72,51 @@ void operator delete[](void* p, size_t size) noexcept {
 }
 
 extern "C" {
-  void* my_alloc(size_t size) {
-    size_t new_size = nallocx(size, 0);
+  void* my_alloc(size_t size) __THROW {
+    size_t new_size = tc_nallocx(size, 0);
     void* p = tc_malloc(size);
     mem_tracker.sum += new_size;
     return p;
   }
 
-  void my_free(void* p) {
+  void my_free(void* p) __THROW {
     size_t size = tc_malloc_size(p);
     tc_free(p);
     mem_tracker.sum -= size;
+  }
+
+  void* my_realloc(void* p, size_t size) __THROW {
+    int64_t old_size = 0;
+    if (p != 0) {
+      old_size = tc_malloc_size(p);
+    }
+    int64_t new_size = tc_nallocx(size, 0);
+    mem_tracker.sum += (new_size - old_size);
+    return tc_realloc(p, size);
+  }
+
+  void* my_calloc(size_t n, size_t size) __THROW {
+    size_t real_size = n * size;
+    size_t new_size = tc_nallocx(real_size, 0);
+    void* p = tc_calloc(n, size);
+    mem_tracker.sum += new_size;
+    return p;
+  }
+
+  void* my_memalign(size_t align, size_t size) __THROW {
+    size_t new_size = tc_nallocx(size, 0);
+    void* p = tc_memalign(align, size);
+    mem_tracker.sum += new_size;
+    return p;
   }
 }
 
 extern "C" {
   void* malloc(size_t size) __THROW ALIAS(my_alloc);
   void free(void* p) __THROW ALIAS(my_free);
+  void* realloc(void* p, size_t size) __THROW ALIAS(my_realloc);
+  void* calloc(size_t n, size_t size) __THROW ALIAS(my_calloc);
+  void* memalign(size_t align, size_t s) __THROW ALIAS(my_memalign);
 }
 
 class C1 {
@@ -109,10 +140,6 @@ int main() {
   auto* c2 = new (std::nothrow) C1();
   delete c2;
 
-  std::cout<<"================New With Exception Align================"<<std::endl;
-  auto* c3 = new (std::align_val_t{16}) C1();
-  delete c3;
-
   std::cout<<"================New Array With Exception================"<<std::endl;
   auto* c4 = new C1[5];
   delete[] c4;
@@ -121,14 +148,42 @@ int main() {
   auto* c5 = new (std::nothrow) C1[5];
   delete[] c5;
 
-  /*
-  std::cout<<"START 3"<<std::endl;
+  std::cout<<"================malloc/free================"<<std::endl;
   std::cout<<"T1:"<<mem_tracker.sum<<std::endl;
-  void* c2 = malloc(10);
+  void* c6 = malloc(10);
   std::cout<<"T2:"<<mem_tracker.sum<<std::endl;
-  std::cout<<"T4:"<<tc_malloc_size(c2)<<std::endl;
+  std::cout<<"T3:"<<tc_malloc_size(c6)<<std::endl;
   free(c2);
-  std::cout<<"T3:"<<mem_tracker.sum<<std::endl;
+  std::cout<<"T4:"<<mem_tracker.sum<<std::endl;
+
+  std::cout<<"================realloc================"<<std::endl;
+  std::cout<<"T1:"<<mem_tracker.sum<<std::endl;
+  void* c7 = malloc(10);
+  std::cout<<"T2:"<<mem_tracker.sum<<std::endl;
+  std::cout<<"T3:"<<tc_malloc_size(c7)<<std::endl;
+  c7 = realloc(c7, 19);
+  std::cout<<"T4:"<<mem_tracker.sum<<std::endl;
+  std::cout<<"T5:"<<tc_malloc_size(c7)<<std::endl;
+  free(c7);
+  std::cout<<"T6:"<<mem_tracker.sum<<std::endl;
+
+  std::cout<<"================calloc================"<<std::endl;
+  std::cout<<"T1:"<<mem_tracker.sum<<std::endl;
+  void* c8 = calloc(10, 1);
+  std::cout<<"T2:"<<mem_tracker.sum<<std::endl;
+  std::cout<<"T3:"<<tc_malloc_size(c8)<<std::endl;
+  free(c8);
+  std::cout<<"T4:"<<mem_tracker.sum<<std::endl;
+
+  std::cout<<"================mem_algin================"<<std::endl;
+  std::cout<<"T1:"<<mem_tracker.sum<<std::endl;
+  void* c9 = memalign(8, 15);
+  std::cout<<"T2:"<<mem_tracker.sum<<std::endl;
+  std::cout<<"T3:"<<tc_malloc_size(c9)<<std::endl;
+  free(c9);
+  std::cout<<"T4:"<<mem_tracker.sum<<std::endl;
+
+  /*
   auto* c3 = new C1();
   */
 }
